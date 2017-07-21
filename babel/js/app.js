d9f1946183fe2ade2b43e9bc59383e31c5711ce1
wmt-php4 <php4.webmob@gmail.com>
2017-07-21 15:44:37 +0530

    commit after webpack demo

--- babel/js/app.js
+++ babel/js/app.js
@@ -1,1 +1,27 @@
-alert("hello world!");
+/*setTimeout(()=>{
+    alert('hello');
+},3000);*/
+/*
+
+//function declaration
+let printMsg = (msg)=>{
+    alert(msg);
+};
+setTimeout(()=>{
+    printMsg("hello");
+},3000);*/
+
+/*
+let userInfo = {
+    'name': 'bhavik',
+    'city': 'ahmedabad',
+    'age': 23
+};
+
+//how to access keys in babel
+let{name,age} =userInfo;
+alert (name + age);*/
+
+import {square,addition} from "./lib.js";
+
+alert(addition(10,10));
---
+++ babel/js/lib.js
@@ -0,0 +1,6 @@
+export function square(a) {
+    return a/a;
+}
+export function addition(a,b) {
+    return a+b;
+}
